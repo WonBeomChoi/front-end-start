4056465e7611c4a4627139a32e3212d999706858
Won beom <cwb2310@naver.com>
2018-09-21 17:58:30 +0900

    다른방법찾아서 다시해바야디

--- 04/app.js
+++ 04/app.js
@@ -2,10 +2,18 @@
     var wrap = document.querySelector(".wrap")
     for(var i=0; i<4; i++){
         for(var j=0; j<4; j++){ 
-            var w = document.createElement('div');
-            var b = document.createElement('div');
-            w.className="w";
-            b.className="b";
-            (i+j)%2==1 ? wrap.appendChild(w): wrap.appendChild(b)
+            var div = document.createElement('div');
+            div.className = (i+j)%2 ==1 ? "w":"b"
+            wrap.appendChild(div);
         }
+    }
+    var blocks = document.querySelectorAll('.wrap > div');
+    
+    
+    function select(event){
+        var block = event.currentTarget;
+        block.style.backgroundColor = 'yellow';
+    }
+    for ( var i = 0; i <16 ; i++){
+        blocks[i].addEventListener('click', select);
     }
